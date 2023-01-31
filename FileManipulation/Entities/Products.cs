67f03602437be528b24eefb5734c2ff0b4f42790
Alexandre M Telles <alexandre.mtelles@gmail.com>
2023-01-31 11:13:31 -0300

    Learing file manipulation

---
+++ FileManipulation/Entities/Products.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace FileManipulation.Entities
+{
+    class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Products() { }
+        public Products(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price*Quantity;
+        }
+
+    }
+}
